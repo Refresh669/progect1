7f4f28f0f81d23305fb37e41e7828830d67cfecc
refresh669 <vlad27171@gmail.com>
2023-02-28 22:29:16 +0200

    first3

---
+++ 3/Script.js
@@ -0,0 +1,17 @@
+// task 1
+
+let admin
+let name = "John"
+admin = name;
+
+alert(admin)
+
+// task 2
+
+let eartName;
+let currentUserName
+
+// task 3
+
+let someVariable = "10" + 10;
+console.log(typeof someVariable)
